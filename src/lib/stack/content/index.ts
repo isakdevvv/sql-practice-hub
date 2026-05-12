53cc6af1e073a8dd70de5baee16ecdf15fbe8310
isakdevvv <isakdevvv@gmail.com>
2026-05-13 00:50:33 +0200

    feat(tek-1501): hypotesetest, kji2 og regresjon med drag-bar scatter

--- src/lib/stack/content/index.ts
+++ src/lib/stack/content/index.ts
@@ -108,6 +108,12 @@ import { content as tek1Deskriptiv } from "./tek1-deskriptiv";
 import { content as tek1Sannsynlighet } from "./tek1-sannsynlighet";
 import { content as tek1Fordelinger } from "./tek1-fordelinger";
 import { content as tek1StatistiskAnalyse } from "./tek1-statistisk-analyse";
+import { content as tek1Kombinatorikk } from "./tek1-kombinatorikk";
+import { content as tek1DiskreteFordelinger } from "./tek1-diskrete-fordelinger";
+import { content as tek1KontinuerligeFordelinger } from "./tek1-kontinuerlige-fordelinger";
+import { content as tek1ForventningClt } from "./tek1-forventning-clt";
+import { content as tek1EstimeringKi } from "./tek1-estimering-ki";
+import { content as tek1HypotesetestRegresjon } from "./tek1-hypotesetest-regresjon";
 import type { TrinnContent } from "../types";
 
 export const TRINN: TrinnContent[] = [
@@ -135,7 +141,9 @@ export const TRINN: TrinnContent[] = [
   traer, graferDypere, hashingDypere, dynamicProgramming,
   dte2509, brukerhandtering,
   tek1501, tek1Deskriptiv, tek1Sannsynlighet, tek1Fordelinger,
-  tek1StatistiskAnalyse,
+  tek1StatistiskAnalyse, tek1Kombinatorikk, tek1DiskreteFordelinger,
+  tek1KontinuerligeFordelinger, tek1ForventningClt, tek1EstimeringKi,
+  tek1HypotesetestRegresjon,
 ];
 
 import { curriculumIndexOf } from "../curriculum";
